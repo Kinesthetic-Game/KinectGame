265bc12c3940712be187b786564dd17baefd2f1d
IvyChooNie <72536620+IvyChooNie@users.noreply.github.com>
2020-11-06 23:13:55 +0800

    Add files via upload

---
+++ ClickHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+[RequireComponent(typeof(Collider))]
+public class ClickHandler : MonoBehaviour
+{
+
+    public UnityEvent upEvent;
+    public UnityEvent downEvent;
+
+    void OnMouseUp()
+    {
+        Debug.Log("Up");
+        upEvent?.Invoke();
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Down");
+        downEvent?.Invoke();
+    }
+}
